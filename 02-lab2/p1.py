1c51a6f29488dbb57d3bbf512e7d1308f378949e
JakubK7 <JakubK7@users.noreply.github.com>
2025-10-20 13:37:05 +0200

    initial commit

--- 02-lab2/p1.py
+++ 02-lab2/p1.py
@@ -1,4 +1,7 @@
-dane = input('Podaj ciąg liczb całkwowitych oddzielonych spacjmi')
+import matplotlib.pyplot as plt
+
+
+dane = input('Podaj ciąg liczb całkowitych oddzielonych spacjami')
 
 if dane.strip() == '':
    print('Nie wprowadzono żadnych liczb!!')
@@ -18,18 +21,41 @@ else:
     max = max(liczby)
     min = min(liczby)
 
+
     print('\n--- Przekształcanie i analiza liczb ---')
     print(f'Lista liczb: {liczby}')
     print(f'Ilość liczb: {ilosc}')
     print(f'Suma: {suma}')
     print(f'Średnia: {srednia}')
     print(f'Ilość liczb dodatnich: {dodatnie}')
-    print(f'Ilość liczb ujemnych: {ujemne}')
+    print(f'Ilość lic zb ujemnych: {ujemne}')
     print(f'Ilość zer: {zera}')
     print(f'Największa liczba: {max}')
     print(f'Najmniejsza liczba: {min}')
 
+    plt.subplot(1,3,1)
+    plt.hist(liczby, bins=10, edgecolor='pink', color='blue')
+    plt.title('Histogram liczb całkowitych')
+    plt.xlabel('Wartość')
+    plt.ylabel('Częstość')
+    plt.show()
+
+    plt.subplot(1,3,2)
+    cat = ['Dodatnie', 'Ujemne', 'Zera']
+    wartosci = [dodatnie, ujemne, zera]
+    plt.bar(cat, wartosci, color=['green', 'red', 'blue'])
+    plt.title('Liczba wystąpień liczb dodatnich, ujemnych i zer')
+    plt.show()
+
+    plt.subplot(1,3,3)
+    plt.plot(range(1, len(liczby) + 1), liczby, marker='o', linestyle='-', color='purple')
+    plt.title('Kolejność wprowadzania liczb')
+    plt.xlabel('Indeks kolejności')
+    plt.ylabel('Wartość liczby')
+    plt.grid(True)
+    plt.show()
+
    except ValueError:
-    print('Błąd: prosze wprowadzić tylko liczby całkowite oddzielone spacjami.')
+    print('Błąd: proszę wprowadzić tylko liczby całkowite oddzielone spacjami.')
 
 
